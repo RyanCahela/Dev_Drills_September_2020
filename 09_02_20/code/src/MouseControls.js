fd5d2f65919fa5ae96d6178aaecfd51d549bd0e3
Ryan Cahela <ryancahela@gmail.com>
2020-09-02 20:46:40 -0700

    keyboard and mouse controls work

--- 09_02_20/code/src/MouseControls.js
+++ 09_02_20/code/src/MouseControls.js
@@ -5,6 +5,68 @@ class MouseControls {
         "You must pass in a canvas element to the MouseControls contrusctro"
       );
     }
+    this.position = { x: 0, y: 0 };
+    this.canvasEl = canvasEl;
+    this.pressed = false;
+    this.released = false;
+    this.isDown = false;
+    this.canvasRect = {};
+    canvasEl.addEventListener(
+      "mousedown",
+      (e) => this.handleMousedown(e),
+      false
+    );
+    canvasEl.addEventListener("mouseup", (e) => this.handleMosueup(e), false);
+    canvasEl.addEventListener(
+      "mousemove",
+      (e) => this.handleMousemove(e),
+      false
+    );
+
+    window.addEventListener(
+      "load",
+      () => this.getCanvasRect(this.canvasEl),
+      false
+    );
+
+    window.addEventListener(
+      "resize",
+      () => this.getCanvasRect(this.canvasEl),
+      false
+    );
+  }
+
+  handleMosueup() {
+    this.released = true;
+    this.isDown = false;
+  }
+
+  handleMousedown() {
+    this.pressed = true;
+    this.isDown = true;
+  }
+
+  handleMousemove(e) {
+    this.getMousePositionFromEvent(e);
+  }
+
+  getMousePositionFromEvent({ clientX, clientY }) {
+    const { position, canvasEl, canvasRect } = this;
+    const xRatio = canvasEl.width / canvasEl.clientWidth;
+    const yRatio = canvasEl.height / canvasEl.clientHeight;
+
+    position.x = (clientX - canvasRect.left) * xRatio;
+    position.y = (clientY - canvasRect.top) * yRatio;
+  }
+
+  update() {
+    this.pressed = false;
+    this.released = false;
+  }
+
+  getCanvasRect(canvasEl) {
+    console.log("getBoundingClientRect Called");
+    this.canvasRect = canvasEl.getBoundingClientRect();
   }
 }
 
--- 09_02_20/code/src/main.js
+++ 09_02_20/code/src/main.js
@@ -1,10 +1,15 @@
 import KeyboardControls from "./KeyboardControls";
+import MouseControls from "./MouseControls";
 
 const canvas = document.createElement("canvas");
 canvas.height = 480;
 canvas.width = 640;
+const keyboardInput = new KeyboardControls();
+const mouseInput = new MouseControls(canvas);
 
 document.getElementById("board").appendChild(canvas);
+canvas.addEventListener("mouseenter", () => (controls = mouseInput), false);
+canvas.addEventListener("mouseleave", () => (controls = keyboardInput), false);
 
 const ctx = canvas.getContext("2d");
 const { height, width } = ctx.canvas;
@@ -12,7 +17,7 @@ const { height, width } = ctx.canvas;
 let t = 0;
 let dt = 0;
 let timeOfLastFrame = 0;
-let controls = new KeyboardControls();
+let controls = keyboardInput;
 let playerX = 0;
 let playerY = 0;
 let playerSpeed = 500;
@@ -23,8 +28,14 @@ function loop(ms) {
   dt = t - timeOfLastFrame;
   timeOfLastFrame = t;
 
-  playerX += controls.x * playerSpeed * dt;
-  playerY += controls.y * playerSpeed * dt;
+  if (controls?.position === undefined) {
+    playerX += controls.x * playerSpeed * dt;
+    playerY += controls.y * playerSpeed * dt;
+  } else {
+    playerX = controls.position.x;
+    playerY = controls.position.y;
+    controls.update();
+  }
 
   ctx.fillStyle = "orange";
   ctx.fillRect(0, 0, width, height);
