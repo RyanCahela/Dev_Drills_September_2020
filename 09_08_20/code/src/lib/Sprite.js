ba17db983f3e9ee300c0920ed50a912696b95b72
Ryan Cahela <ryancahela@gmail.com>
2020-09-08 11:55:16 -0700

    built a working prototype game

---
+++ 09_08_20/code/src/lib/Sprite.js
@@ -0,0 +1,8 @@
+class Sprite {
+  constructor(texture) {
+    this.position = { x: 0, y: 0 };
+    this.texture = texture;
+  }
+}
+
+export default Sprite;
---
+++ 09_08_20/code/src/lib/index.js
@@ -0,0 +1,8 @@
+import Container from "./Container";
+import Text from "./Text";
+import CanvasRenderer from "./CanvasRenderer";
+import Texture from "./Texture";
+import Sprite from "./Sprite";
+import KeyboardControls from "./KeyboardControls";
+
+export { Container, Text, CanvasRenderer, Texture, Sprite, KeyboardControls };
