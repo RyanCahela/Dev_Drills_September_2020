3bcf6085da07e5805739a8ffff1d88e5a6c38c5b
Ryan Cahela <ryancahela@gmail.com>
2020-09-10 10:17:16 -0700

    basic animation technique section finished, moving on to more advanced techniques

--- 09_10_20/practice_1/src/main.js
+++ 09_10_20/practice_1/src/main.js
@@ -2,6 +2,7 @@ import Game from "./lib/Game";
 import Texture from "./lib/Texture";
 import TileSprite from "./lib/TileSprite";
 import Squizz from "./entities/Squizz";
+import { randomInt, randomFloat, randomOneIn } from "./utils/math";
 
 const WIDTH = 640;
 const HEIGHT = 480;
@@ -12,12 +13,13 @@ const textures = {
   squizz: new Texture("./resources/player-walk.png"),
 };
 
-const squizz = new Squizz(0.1, 4);
-const squizz2 = new Squizz(0.5, 4);
-
-squizz2.position.x = 50;
-
-myGame.add(squizz);
-myGame.add(squizz2);
+for (let i = 0; i < 1000; i++) {
+  const animationSpeed = randomFloat(0, 1);
+  const numberOfFrames = 4;
+  const squizz = new Squizz(animationSpeed, numberOfFrames);
+  squizz.position.x = randomInt(WIDTH);
+  squizz.position.y = randomInt(HEIGHT);
+  myGame.add(squizz);
+}
 
 myGame.run(() => {});
